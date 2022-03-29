bb8f79feadb527629ccd158b67d9e9ad58c4f5a4
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-03-29 14:37:23 -0700

    Add bootstrap

--- controllers/skills.js
+++ controllers/skills.js
@@ -1,11 +1,18 @@
 const Skill = require('../models/skill');
 
 module.exports = {
-    index
+    index,
+    show
   };
   
   function index(req, res) {
     res.render('skills/index', {
       skills: Skill.getAll()
+    });
+  }
+
+  function show(req, res) {
+    res.render('skills/show', {
+      s: Skill.getOne(req.params.id)
     });
   }
--- models/skill.js
+++ models/skill.js
@@ -1,13 +1,25 @@
 const skills = [
-    {id: 125223, todo: 'Feed Dogs', done: true},
-    {id: 127904, todo: 'Learn Express', done: false},
-    {id: 139608, todo: 'Buy Milk', done: false}
+    {id: 125223, skill: 'Pseudocoding', difficulty: 'easy'},
+    {id: 127904, skill: 'MVC Architecture', difficulty: 'medium'},
+    {id: 139608, skill: 'HTTP Request Types', difficulty: 'easy'},
+    {id: 131154, skill: 'Version Control', difficulty: 'medium'},
+    {id: 147623, skill: 'HTML5 & CSS3', difficulty: 'medium'},
+    {id: 122298, skill: 'Node.js & Express', difficulty: 'hard'},
+    {id: 139111, skill: 'MongoDB', difficulty: 'hard'},
+    {id: 149285, skill: 'Authentication', difficulty: 'hard'}
   ];
   
   module.exports = {
-    getAll
+    getAll,
+    getOne
   };
   
   function getAll() {
     return skills;
   }
+
+  function getOne(id) {
+    // Use the Array.prototype.find iterator method
+    return skills.find(skills => skills.id === parseInt(id));
+  }
+ 
