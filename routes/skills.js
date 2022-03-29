495d8f7024fe45cc4d97dc5e346949953aedeaa9
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-03-29 13:44:09 -0700

    Add controller

--- routes/skills.js
+++ routes/skills.js
@@ -1,14 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+const Skill = require('../models/skill');
 
-app.get('/skills', function(req, res) {
+router.get('/', function(req, res) {
   res.render('skills/index', {
-    todos: skillDb.getAll()
+    todos: Skill.getAll()
   });
 });
 
