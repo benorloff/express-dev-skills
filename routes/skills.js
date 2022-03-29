a50dd4859993a8d32023c786fc9059ad48d2655b
Ben Orloff <99829882+benorloff@users.noreply.github.com>
2022-03-29 13:36:26 -0700

    refactor routes

--- routes/skills.js
+++ routes/skills.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+app.get('/skills', function(req, res) {
+  res.render('skills/index', {
+    todos: skillDb.getAll()
+  });
+});
+
 module.exports = router;
